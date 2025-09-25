cbeaef8c2d10cf519c43d8df16f77311848f1172
arcadyrajeev <bitmasteratj@gmail.com>
2025-09-25 22:26:40 +0530

    responsive

--- app/components/NewsSection.tsx
+++ app/components/NewsSection.tsx
@@ -45,22 +45,22 @@ const newsItems = [
 
 export default function NewsSection() {
   return (
-    <section className="w-full bg-white py-16 pb-50">
-      <div className="w-full mx-auto px-4 sm:px-6 lg:px-10">
+    <section className="w-full bg-white py-[6vw] pb-50">
+      <div className="w-full mx-auto px-4 sm:px-6 lg:px-[2vw]">
         {/* Header */}
-        <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-20">
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-[6vw]">
           <h2 className="text-[5vw]  text-black">Our News</h2>
-          <p className="mt-4 md:mt-0 max-w-[24%] text-justify text-gray-600 text-lg">
+          <p className="mt-4 md:mt-0 w-full md:w-[24%] text-justify text-gray-600 text-lg md:text-[1.3vw]">
             From new product drops to style tips — read our latest features,
             editorials, and brand announcements.
           </p>
         </div>
 
         {/* News Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-[1.4vw]">
           {newsItems.map((item, idx) => (
             <Link href={item.link} key={idx} className="group cursor-pointer">
-              <div className="relative w-full  h-140 overflow-hidden rounded-md bg-gray-200">
+              <div className="relative w-full h-120 md:h-[38vw] overflow-hidden rounded-md bg-gray-200">
                 <Image
                   src={item.image}
                   alt={item.title}
@@ -68,10 +68,12 @@ export default function NewsSection() {
                   className="object-cover group-hover:scale-110 transition-transform duration-500"
                 />
               </div>
-              <h3 className="mt-4 text-lg font-semibold text-black">
+              <h3 className="mt-4 text-lg md:text-[1.6vw] font-semibold text-black">
                 {item.title}
               </h3>
-              <p className="text-sm text-gray-600">{item.description}</p>
+              <p className="text-sm md:text-[1vw] text-gray-600">
+                {item.description}
+              </p>
             </Link>
           ))}
         </div>
