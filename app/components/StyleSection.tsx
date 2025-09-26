07f55ad3e19215776d48a8f06c2917916e198109
arcadyrajeev <bitmasteratj@gmail.com>
2025-09-26 17:58:33 +0530

    home update

--- app/components/StyleSection.tsx
+++ app/components/StyleSection.tsx
@@ -14,22 +14,22 @@ export default function StyleSection() {
 
   const scaleImage1 = useTransform(scrollYProgress, [0, 0.5], [0.1, 1]);
   const scaleImage2 = useTransform(scrollYProgress, [0.4, 0.6], [0.5, 1]);
-  const scaleImage3 = useTransform(scrollYProgress, [0.5, 0.7], [0.4, 1]);
-  const scaleImage4 = useTransform(scrollYProgress, [0.7, 1], [0.5, 1]);
+  const scaleImage3 = useTransform(scrollYProgress, [0.3, 0.5], [0.4, 1]);
+  const scaleImage4 = useTransform(scrollYProgress, [0.4, 1], [0.5, 1]);
 
   return (
     <section
       ref={containerRef}
       className="relative flex flex-col z-6 w-full min-h-[400dvh] bg-white flex "
     >
-      <div className="w-full  sticky top-0 h-[100dvh] bg-white flex items-center justify-center">
-        <h1 className="text-5xl md:text-[5vw] text-center w-full md:w-[30%] text-black ">
+      <div className="w-full  sticky top-0 h-[100dvh] bg-white flex items-center justify-center ">
+        <h1 className="text-6xl md:text-[7vw] text-center w-full md:w-[40%] text-black ">
           Style It <br /> Your Way
         </h1>
       </div>
       <motion.div
         style={{ scale: scaleImage1 }}
-        className="absolute top-[30%] left-[35%] bg-gray-600 w-[28vw] h-[36vw]"
+        className="absolute top-[25%] left-[35%] bg-gray-600 w-[28vw] h-[36vw]"
       >
         <Image
           src="/images/style1.avif"
@@ -40,7 +40,7 @@ export default function StyleSection() {
       </motion.div>
       <motion.div
         style={{ scale: scaleImage2 }}
-        className="absolute top-[50%] left-[70%] bg-gray-600 w-[24vw] h-[30vw]"
+        className="absolute top-[42%] left-[75%] bg-gray-600 w-[24vw] h-[30vw]"
       >
         <Image
           src="/images/style2.avif"
@@ -51,7 +51,7 @@ export default function StyleSection() {
       </motion.div>
       <motion.div
         style={{ scale: scaleImage3 }}
-        className="absolute top-[60%] left-[5%] bg-gray-600 w-[24vw] h-[30vw]"
+        className="absolute top-[45%] left-[2%] bg-gray-600 w-[28vw] h-[36vw]"
       >
         <Image
           src="/images/style3.avif"
@@ -62,7 +62,7 @@ export default function StyleSection() {
       </motion.div>
       <motion.div
         style={{ scale: scaleImage4 }}
-        className="absolute top-[80%] left-[30%] bg-gray-600 w-[36vw] h-[55vw]"
+        className="absolute top-[65%] z-8 left-[30%] bg-gray-600 w-[40vw] h-[50vw]"
       >
         <Image
           src="/images/style4.avif"
@@ -71,6 +71,7 @@ export default function StyleSection() {
           className="w-full h-full object-cover"
         />
       </motion.div>
+      <div className="w-full absolute z-5 bottom-0 h-[24vw] bg-white"></div>
     </section>
   );
 }
